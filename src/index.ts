75a1853e5f94b68cd138410ee92cfe0f40947c07
Joe Luttrell <joe@readytensor.com>
2024-03-21 16:40:50 +0100

    add comment

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,4 @@ import type { ZodTypeProvider } from "fastify-type-provider-zod";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
-export const handler = fastifyLambda(app);
+export const handler = fastifyLambda(app); //type error here on app
